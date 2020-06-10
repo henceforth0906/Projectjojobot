38ee7fa808fd8ef1a69b55e67a3ef36555d42654
henceforth0906 <56990807+henceforth0906@users.noreply.github.com>
2020-06-10 18:33:28 +0900

    Update py.py

--- py.py
+++ py.py
@@ -2,17 +2,14 @@ import discord
 
 client = discord.Client()
 
-
 @client.event
 async def on_ready():
     print(client.user.id)
     print("ready")
 
-
 @client.event
 async def on_message(message):
     if message.content.startswith("안녕"):
         await message.channel.send("눼눼")
-
 
 client.run("NzIwMTcxODY0NDU4MjY0NjY2.XuCfXQ.MF7srlgbTlmGzBwEiyP4EkUa2pk")
